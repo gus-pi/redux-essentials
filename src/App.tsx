96c942b05f70453349967a41d82b06b5f01f4cfd
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-13 15:54:33 -0400

    add single post page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import PostsList from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsMainPage } from './features/posts/PostsMainPage'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -10,17 +12,8 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <section>
-                <>
-                  <AddPostForm />
-                  <PostsList />
-                </>
-              </section>
-            }
-          ></Route>
+          <Route path="/" element={<PostsMainPage />} />
+          <Route path="/posts/:postId" element={<SinglePostPage />} />
         </Routes>
       </div>
     </Router>
