20e5f22d294587226f4883fe48e1e186560c479e
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-13 15:59:23 -0400

    add postUpdated action

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -6,7 +6,6 @@ const PostsList = () => {
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>
-        {' '}
         <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
