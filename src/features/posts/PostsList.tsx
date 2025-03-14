c810fc8e0a982c8cc2e1bbc849777e36e39be9c2
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-13 23:30:44 -0400

    sort posts list

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -6,7 +6,10 @@ import { TimeAgo } from '@/components/TimeAgo'
 
 const PostsList = () => {
   const posts = useAppSelector(selectAllPosts)
-  const renderedPosts = posts.map((post) => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>
         <Link to={`/posts/${post.id}`}>{post.title}</Link>
