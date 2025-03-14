e4950e4dc8e04bf861bda2cf20a20c164f9c843b
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-14 01:20:06 -0400

    use extra reducer in posts slice

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,6 +1,7 @@
 import { RootState } from '@/app/store'
 import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
 import { sub } from 'date-fns'
+import { userLoggedOut } from '../auth/authSlice'
 
 export interface Reactions {
   thumbsUp: number
@@ -88,6 +89,13 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++
       }
     },
+  },
+  extraReducers: (builder) => {
+    // Pass the action creator to `builder.addCase()`
+    builder.addCase(userLoggedOut, (state) => {
+      // Clear out the list of posts whenever the user logs out
+      return []
+    })
   },
 })
 
