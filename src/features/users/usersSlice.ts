e1919d8fa40dc6b892ba2b81886a27fa8de07754
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-16 19:40:52 -0400

    fetch users with thunk

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,22 +1,30 @@
 import { RootState } from '@/app/store'
 import { createSlice } from '@reduxjs/toolkit'
 import { selectCurrentUsername } from '../auth/authSlice'
+import { createAppAsyncThunk } from '@/app/withTypes'
+import { client } from '@/api/client'
 
 type User = {
   id: string
   name: string
 }
 
-const initialState = <User[]>[
-  { id: '0', name: 'John Johnson' },
-  { id: '1', name: 'Bob Bobson' },
-  { id: '2', name: 'Jack Jackson' },
-]
+export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get<User[]>('/fakeApi/users')
+  return response.data
+})
+
+const initialState: User[] = []
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
